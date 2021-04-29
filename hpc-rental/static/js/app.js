487641eec2c0cae25dea60bceefd6a0d09b06504
NieceyX <deniece@radfords.net>
2021-04-28 19:08:55 -0500

    added first react stuff

---
+++ hpc-rental/static/js/app.js
@@ -0,0 +1,206 @@
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            view: 'login'
+        };
+    }
+
+    onLogin() {
+        this.setState({
+            view: 'avengers'
+        });
+    }
+    onGoCreate() {
+        this.setState({
+            view: 'createProfile'
+        });
+    }
+    onGoLogin() {
+        this.setState({
+            view: 'login'
+        });
+
+    }
+
+    render() {
+        let component = <Login onLogin={ () => this.onLogin() } />;
+        if (this.state.view == 'avengers') {
+            component = <Avengers />;
+        }
+        else if (this.state.view == 'createProfile') {
+            component = <ProfileCreate />;
+        }
+
+        return (
+            <div className="app">
+                {component}
+            </div>
+        );
+    }
+}
+
+class Login extends React.Component {
+    sendLoginRequest() {
+        let formData = new FormData(document.getElementById('login-form'));
+        fetch('/api/login/', {
+            method: 'POST',
+            body: formData
+        })
+        .then(result => result.text())
+        .then(
+            (result) => {
+                if (result == 'ok') {
+                    this.props.onLogin();
+                } else {
+                    alert('Bad username/password combo.');
+                }
+            },
+            (error) => {
+                alert('General login error');
+            }
+        );
+    }
+
+    render() {
+        return (
+          <div>
+            <form id="login-form">
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="username" />
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="password" />
+                <br />
+                <button id="login-button" onClick={(evt) => {
+                    evt.preventDefault();
+                    this.sendLoginRequest();
+                }}>Login</button>
+            </form>
+            <p style={styles.curserPointer} onClick={() => {
+                  this.props.onGoCreate()  }> 
+                No profile? Make one here! </p>
+          </div>
+        );
+    }
+}
+
+class ProfileCreate extends React.Component {
+    sendLoginRequest() {
+        let formData = new FormData(document.getElementById('profile-form'));
+        fetch('/api/create-profile/', {
+            method: 'POST',
+            body: formData
+        })
+        .then(result => result.text())
+        .then(
+            (result) => {
+                if (result == 'ok') {
+                    this.props.onLogin();
+                } else {
+                    alert('Bad username/password combo.');
+                }
+            },
+            (error) => {
+                alert('General login error');
+            }
+        );
+    }
+
+    render() {
+        return (
+          <div>
+            <form id="profile-form">
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  placeholder="email" />
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="username" />
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="password" />
+                <br />
+                <button id="create-button" onClick={(evt) => {
+                    evt.preventDefault();
+                    this.sendLoginRequest();
+                }}>Create Profile</button>
+            </form>
+
+            <p style={styles.curserPointer} onClick={() => {
+                  this.props.onGoLogin()  }> 
+                Go back to login! </p>
+
+          </div>
+        );
+    }
+}
+
+
+class Avengers extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            avengers: [],
+            isLoaded: false,
+            error: null 
+        };
+    }
+
+    componentDidMount() {
+        fetch('/api/avengers/')
+        .then(result => result.json())
+        .then(
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                    avengers: result
+                });                
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error: error
+                });
+            }
+        );
+    }
+
+    render() {
+        if (this.state.error) {
+            return <div>Error! Avengers must have gotten snapped by Thanos!</div>
+        } else if (!this.state.isLoaded) {
+            return <div>Waiting for Avengers to assemble...</div>
+        } else {
+            return (
+                <div className="avengers">
+                    <h1>Avengers Assembled!</h1>
+                    <ul>
+                        {this.state.avengers.map(hero => (
+                        <li key={hero}>
+                            {hero}
+                        </li>
+                        ))}
+                    </ul>
+                </div>
+            );
+        }
+
+    }
+}
+
+
+ReactDOM.render(<App />, document.querySelector('#content'));
